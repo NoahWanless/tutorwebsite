1a700bced464917df6578524a0bad21ff2530f49
Kellan-Stempin <stempinkellan@gmail.com>
2025-10-17 12:33:33 -0600

    SignUp/Login Styled and structured, logout functionality added

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm #mabye get different one
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
+from django.contrib import messages
+from django.views import View
 from .forms import TutorSignUp
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 class SignUpView(CreateView):
-    form_class = TutorSignUp #!this is custom 
+    form_class = TutorSignUp
     success_url = reverse_lazy('login')
     template_name = "registration/signup.html"
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Account created successfully! Please log in.')
+        return response
+
+class LogoutView(View):
+    def post(self, request):
+        logout(request)
+        messages.success(request, 'You have been logged out successfully.')
+        return redirect('home')
