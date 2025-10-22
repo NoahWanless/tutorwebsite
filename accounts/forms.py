9d002511025b49ff01dcb9a41520886f1b01d818
Toby Simonsen <ts182680@umconnect.umt.edu>
2025-10-22 15:22:30 -0600

    Tutor Details

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,55 +4,93 @@ from django import forms
 from django.contrib.auth import get_user_model
 
 
-
 class TutorSignUp(UserCreationForm):
-    #first_name = forms.CharField(max_length=20)
-    #last_name = forms.CharField(max_length=20)
-    #email = forms.EmailField()
-    #username
-    #password.      both these automatically are there
-    class Meta: #get_user_model()
+    # first_name = forms.CharField(max_length=20)
+    # last_name = forms.CharField(max_length=20)
+    # email = forms.EmailField()
+    # username
+    # password.      both these automatically are there
+    class Meta:  # get_user_model()
         model = tutors
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2','days_tutor_for','classes_tutor_for','hours_tutor_for_mon',
+        """fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2','days_tutor_for','classes_tutor_for','hours_tutor_for_mon',
                   'hours_tutor_for_tue','hours_tutor_for_wed','hours_tutor_for_thr','hours_tutor_for_fri','hours_tutor_for_sat',
-                  'hours_tutor_for_sun')
+                  'hours_tutor_for_sun')"""
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password1",
+            "password2",
+            "days_tutor_for",
+            "classes_tutor_for",
+        )
         help_texts = {
-            'username': None,
+            "username": None,
         }
 
 
 class AccountDetailsForm(forms.ModelForm):
     """Form for updating basic account information"""
+
     class Meta:
         model = tutors
-        fields = ('first_name', 'last_name', 'email')
+        fields = ("first_name", "last_name", "email")
         widgets = {
-            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
         }
 
 
 class AvailabilityForm(forms.ModelForm):
     """Form for updating availability settings"""
+
     class Meta:
         model = tutors
-        fields = ('days_tutor_for', 'classes_tutor_for', 'hours_tutor_for_mon',
-                  'hours_tutor_for_tue', 'hours_tutor_for_wed', 'hours_tutor_for_thr',
-                  'hours_tutor_for_fri', 'hours_tutor_for_sat', 'hours_tutor_for_sun')
+        fields = (
+            "days_tutor_for",
+            "classes_tutor_for",
+            "hours_tutor_for_mon",
+            "hours_tutor_for_tue",
+            "hours_tutor_for_wed",
+            "hours_tutor_for_thr",
+            "hours_tutor_for_fri",
+            "hours_tutor_for_sat",
+            "hours_tutor_for_sun",
+        )
         widgets = {
-            'days_tutor_for': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-            'classes_tutor_for': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-            'hours_tutor_for_mon': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-            'hours_tutor_for_tue': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-            'hours_tutor_for_wed': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-            'hours_tutor_for_thr': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-            'hours_tutor_for_fri': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-            'hours_tutor_for_sat': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
-            'hours_tutor_for_sun': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
+            "days_tutor_for": forms.CheckboxSelectMultiple(
+                attrs={"class": "form-check-input"}
+            ),
+            "classes_tutor_for": forms.CheckboxSelectMultiple(
+                attrs={"class": "form-check-input"}
+            ),
+            "hours_tutor_for_mon": forms.CheckboxSelectMultiple(
+                attrs={"class": "form-check-input"}
+            ),
+            "hours_tutor_for_tue": forms.CheckboxSelectMultiple(
+                attrs={"class": "form-check-input"}
+            ),
+            "hours_tutor_for_wed": forms.CheckboxSelectMultiple(
+                attrs={"class": "form-check-input"}
+            ),
+            "hours_tutor_for_thr": forms.CheckboxSelectMultiple(
+                attrs={"class": "form-check-input"}
+            ),
+            "hours_tutor_for_fri": forms.CheckboxSelectMultiple(
+                attrs={"class": "form-check-input"}
+            ),
+            "hours_tutor_for_sat": forms.CheckboxSelectMultiple(
+                attrs={"class": "form-check-input"}
+            ),
+            "hours_tutor_for_sun": forms.CheckboxSelectMultiple(
+                attrs={"class": "form-check-input"}
+            ),
         }
 
-'''
+
+"""
  hours_tutor_for_mon = models.JSONField(blank=True, default=list) 
     hours_tutor_for_tue = models.JSONField(blank=True, default=list) 
     hours_tutor_for_wed = models.JSONField(blank=True, default=list) 
@@ -60,4 +98,4 @@ class AvailabilityForm(forms.ModelForm):
     hours_tutor_for_fri = models.JSONField(blank=True, default=list) 
     hours_tutor_for_sat = models.JSONField(blank=True, default=list) 
     hours_tutor_for_sun = models.JSONField(blank=True, default=list) 
-    '''
+    """
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
 from .models import tutors
+
 
 class HomePageView(TemplateView):
     template_name = "home.html"
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tutors'] = tutors.objects.all()
+        context["tutors"] = tutors.objects.all()
         return context
+
+
+class TutorDetailView(DetailView):
+    model = tutors
+    template_name = "tutor_detail.html"
+    context_object_name = "tutor"
